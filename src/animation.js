4abf51ec76e02859fa0416aee02c7671dc5605ae
jjmadrigap <jjmadrigap@eafit.edu.co>
2022-04-30 10:17:17 -0500

    Update animation
    Create bounce animation

--- src/animation.js
+++ src/animation.js
@@ -6,7 +6,7 @@ var backgroundColour = 0xffffff
 var objectColour = 0xff00ff
 var objectPos = 10
 //to choose object
-var choice = 0
+var choice = 1
 
 //set scene
 const scene = new THREE.Scene();
@@ -56,15 +56,15 @@ function makeSphere(colour, pos){
 //All creation ends at this point
 
 if(choice==0){
-    object = makeCube(objectColour, objectPos)
+    cube = makeCube(objectColour, objectPos)
 }else {
-    object = makeSphere(objectColour, objectPos)
+    sphere = makeSphere(objectColour, objectPos)
 }
 
 
 
 
-
+/*
 function rotationAnimate() {
     requestAnimationFrame( rotationAnimate );
     
@@ -76,3 +76,30 @@ function rotationAnimate() {
     
 };
 rotationAnimate()
+*/
+let acceleration = 9.8;
+let bounce_distance = 9;
+let bottom_position_y = -4;
+let time_step = 0.02;
+// time_counter is calculated to be the time the ball just reached the top position
+// this is simply calculated with the s = (1/2)gt*t formula, which is the case when ball is dropped from the top position
+let time_counter = Math.sqrt(bounce_distance * 2 / acceleration);
+let initial_speed = acceleration * time_counter;
+
+// Animate the scene
+const animate = () => {
+  requestAnimationFrame( animate );
+
+  // reset time_counter back to the start of the bouncing sequence when sphere hits through the bottom position
+  if (sphere.position.y < bottom_position_y) {
+    time_counter = 0;
+  }
+  // calculate sphere position with the s2 = s1 + ut + (1/2)gt*t formula
+  // this formula assumes the ball to be bouncing off from the bottom position when time_counter is zero
+  sphere.position.y = bottom_position_y + initial_speed * time_counter - 0.5 * acceleration * time_counter * time_counter;
+  // advance time
+  time_counter += time_step;
+
+  renderer.render( scene, camera );
+};
+animate();
