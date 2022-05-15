4f414bf7fa751c119124a08a647cfcbd0ee543f0
jjmadrigap <jjmadrigap@eafit.edu.co>
2022-05-14 19:17:49 -0500

    Mouse Controls
    Add mouse control around the object

--- src/choice.js
+++ src/choice.js
@@ -14,6 +14,7 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+const controls = new THREE.OrbitControls ( camera, renderer.domElement );
 
 function setScene(){
     
